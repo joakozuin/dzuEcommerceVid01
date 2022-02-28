e548079567010163e9707285cc5eaa62c8253d26
joaquin zuin <joaquinzuin16@gmail.com>
2022-02-28 19:37:33 -0300

    inicio proyecto

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+const App = () => {
+  
+  return (
+  <div>
+  <Login/>;
+  <Register/>;
+  <Home/>;
+  <Product/>;
+  <ProductList/>;
+  <Cart/>;
+
+  </div>
+  )
+};
+
+export default App;
